402ac4921f9f82607ee9ac3cdf284cae521719e9
Shahrokh Ashayeri <Ashayeri.eng@gmail.com>
2024-02-06 23:52:12 +0330

    Add teacher class

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ if __name__ == "__main__":
         last_name = input("Enter Last Name: ")
         new_student = Student(f_name=first_name, l_name=last_name)
         students.append(new_student)
-
     r = Report()
     for student in students:
         student.set_scores()
