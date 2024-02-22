f053e644a08d06831828f86e12e690be3d001926
Shahrokh Ashayeri <Ashayeri.eng@gmail.com>
2024-02-22 12:58:43 +0330

    Updat: main file

--- main.py
+++ main.py
@@ -2,24 +2,11 @@ from lib.student import Student
 from lib.classroom import Classroom
 from lib.report import Report
 from lib.file_op import FileOperation
+from lib.menu import Menu
 
 if __name__ == "__main__":
-    students = list()
-    for counter in range(2):
-        first_name = input("Enter First Name: ")
-        last_name = input("Enter Last Name: ")
-        field = input("Field: ")
-        grade = input("Grade: ")
-        new_student = Student(
-            f_name=first_name, l_name=last_name, field=field, grade=grade
-        )
-        students.append(new_student)
-    r = Report()
-    for student in students:
-        student.set_scores()
-        r.show_scores(student)
-
-    field_of_study = input("Field of study for classroom: ")
-    grade = input("Grade of classroom: ")
-    c = Classroom(students=students, field_of_study=field_of_study, grade=grade)
-    print(c.best_scores())
+    command = ""
+    while command.lower() != "q":
+        menu = Menu()
+        command = menu.show()
+        menu.command(command)
